279a71b3fbc47680985aeb5c2f5707d41101bce6
Arowolaju Olatunde Francis <77964339+brel01@users.noreply.github.com>
2021-11-09 18:53:27 -0800

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,45 @@
+let cv  = [{
+  "Fullname": "Arowolaju Olatunde",
+  "Email": "arowolajutunde@gmail.com",
+  "phone": "+2348136430771",
+  "Location": "Lekki phase 2, Lagos State",
+  "Skills": ['html', "css", "javascript", "Bootstrap", "jQuery", "MySql", "React", "NodeJS", "React Native", "PHP", "GIT", "Google Cloud", "Express JS", "Mongo DB", "AWS", "Critical thinking"],
+  "Objective": "Dedicated developer with four years of experience in web and mobile app development, database administation, marketing and crirtcal thinking with strong creative and analytical skills. Team player with an for detail",
+  "Expericence": [
+    {
+      "Company Name": "K-dev",
+      "Start Date": "Febuary 2017",
+      "End Date": "November 2019",
+      "Title": "Fullstack Developer",
+      "History": "Lead a team of 10 to develop a cryptocurency fund raising platform, I also lead a team of 8 to develop a fin-tech app and other projects",
+    },
+    {
+      "Company Name": "Bayo Arowolaju Foundation",
+      "Start Date": "Febuary 2019",
+      "End Date": "November 2020",
+      "Title": "Consultant",
+      "History": "Worked as sotware and graphics desig consultant",
+    },
+    {
+      "Company Name": "K-dev",
+      "Start Date": "November 2019",
+      "End Date": "June 2021",
+      "Title": "Lead javascript developer",
+      "History": "Lead a team of 12 and tranined 4 interns",
+    },
+    
+  ],
+  "Education" : [
+    {
+      "Name": "Ekiti State University",
+      "Award": "Bsc Statistics",      
+    }
+  ],
+  "Refrence" : [
+    {
+      "name": "Kolade Emmanuel",
+      "Title": "CTO K-dev",
+      "email": "cto@k-dev.org",
+    }
+  ],
+}]
