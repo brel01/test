ef286dfe6b2f5dc7b4222cdb530be1b3b9622d05
Arowolaju Olatunde Francis <77964339+brel01@users.noreply.github.com>
2021-11-09 20:25:50 -0800

    Update cv.js

--- cv.js
+++ cv.js
@@ -11,7 +11,7 @@ let cv  = [{
       "Start Date": "Febuary 2017",
       "End Date": "November 2019",
       "Title": "Fullstack Developer",
-      "History": "Lead a team of 10 to develop a cryptocurency fund raising platform, I also lead a team of 8 to develop a fin-tech app and other projects",
+      "History": "Was a memeber of a team invlolved in development/management/maintainance of Point Of Sales Systems, Inventory Management System and Hospital Management Systems. Had the opportunity to lead a team of 10 to develop a cryptocurency fund raising platform, I also lead a team of 8 to develop a fin-tech app and other projects",
     },
     {
       "Company Name": "Bayo Arowolaju Foundation",
@@ -25,7 +25,7 @@ let cv  = [{
       "Start Date": "November 2019",
       "End Date": "June 2021",
       "Title": "Lead javascript developer",
-      "History": "Lead a team of 12 and tranined 4 interns",
+      "History": "Lead a team of 12 to develop fin-tech applications, logistics mobile application and others, and also managed existing products/systems excellently. Tranined 4 interns",
     },
     
   ],
